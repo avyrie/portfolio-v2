1702c00f43eae14dbeb32a73a2ca9c7522dea468
avyrie <pittman.ally@gmail.com>
2020-10-12 12:30:04 -0600

    projects added, resume added

--- js/app.js
+++ js/app.js
@@ -15,37 +15,3 @@ function myFunction() {
       y.className = "fa fa-bars";
     };
 }
-
-// About
-
-function aboutFunction1() {
-  var dots = document.getElementById("dots1");
-  var moreText = document.getElementById("more1");
-  var btnText = document.getElementById("myBtn1");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read More...";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read Less...";
-    moreText.style.display = "inline";
-  }
-}
-
-function aboutFunction2() {
-  var dots = document.getElementById("dots2");
-  var moreText = document.getElementById("more2");
-  var btnText = document.getElementById("myBtn2");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read More...";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read Less...";
-    moreText.style.display = "inline";
-  }
-}
